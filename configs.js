156149744db67a36d547cddaefee6524829d8307
zxx-0 <51950055+zxx-0@users.noreply.github.com>
2021-01-20 16:33:41 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -1,13 +1,13 @@
 YL.static = {
   /** “关于”信息 */
-  softwareName: '风叶的小屋', //网站名。请在此处填写您自己的网站名，如王小明的博客。*
+  softwareName: '风叶的小窝', //网站名。请在此处填写您自己的网站名，如王小明的博客。*
   version: "1.0.0", // 网站版本号
   iconBtnStart: 'html5', //主图标
   author: '风子',//作者*
-  contactInformation: 'null',//联系方式
-  officialWebsite: '//null',//软件官网
+  contactInformation: '//null',//联系方式
+  officialWebsite: 'https://ylui.yuri2.cn/',//软件官网
   welcome: '本网站UI由 YLUI 强力驱动\n更多信息：//ylui.yuri2.cn',//加载完毕控制台提示信息
-  copyrightDetail: 'null',//版权详细信息
+  copyrightDetail: 'YLUI',//版权详细信息
   otherStatements: '',//其他信息（可留空）
 
   /**————————————————————————————————————————————————————————————————————————————————————————————*/
