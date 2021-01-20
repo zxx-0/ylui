d5a971a70e3303223c2ed7f2b6060e0ab32ab37e
zxx-0 <51950055+zxx-0@users.noreply.github.com>
2021-01-20 13:22:14 +0800

    Update yl-render.js

--- res/js/yl-render.js
+++ res/js/yl-render.js
@@ -410,23 +410,17 @@ YL.render = function (data) {
         if (first) YL.onReady(autoRun); else {
           autoRun()
         }
-
+        
+        //此处可以设置首次访问网站时通知中心的推送内容  
         //社区版提示检测
-        if (first && !YL.static.serialNumber) {
-          YL.onReady(function () {
-            setTimeout(function () {
-              YL.msg("YLUI v" + YL.info.version + " 社区版", '当前YLUI为社区版，仅限个人用户学习使用，禁止商用及企业使用。<br/>更多信息及获取商业授权请访问:<br/>YLUI官网：' + '<a style="color: white" target="_blank" href="https://ylui.yuri2.cn">https://ylui.yuri2.cn</a><p>欢迎使用支付宝扫描二维码对作者进行捐赠，表达您对YLUI的支持~<br/><br/><img style="width: 100%" src="./res/img/donation.png"/></p>');
-            }, 1500)
-          });
-        }
-        //TEST
-        if (first && !YL.static.serialNumber) {
-          YL.onReady(function () {
-            setTimeout(function () {
-              YL.msg("YLUI v" + YL.info.version + " 测试版", '当前YLUI为社区版，仅限个人用户学习使用，禁止商用及企业使用。<br/>更多信息及获取商业授权请访问:<br/>YLUI官网：' + '<a style="color: white" target="_blank" href="https://ylui.yuri2.cn">https://ylui.yuri2.cn</a><p>欢迎使用支付宝扫描二维码对作者进行捐赠，表达您对YLUI的支持~<br/><br/><img style="width: 100%" src="./res/img/donation.png"/></p>');
-            }, 1500)
-          });
-        }
+        //if (first && !YL.static.serialNumber) {
+        //  YL.onReady(function () {
+        //    setTimeout(function () {
+        //      YL.msg("YLUI v" + YL.info.version + " 社区版", '当前YLUI为社区版，仅限个人用户学习使用，禁止商用及企业使用。<br/>更多信息及获取商业授权请访问:<br/>YLUI官网：' + '<a style="color: white" target="_blank" href="https://ylui.yuri2.cn">https://ylui.yuri2.cn</a><p>欢迎使用支付宝扫描二维码对作者进行捐赠，表达您对YLUI的支持~<br/><br/><img style="width: 100%" src="./res/img/donation.png"/></p>');
+        //    }, 1500)
+        //  });
+        //}
+
 
         this.ready = true;
       },
